d4b2a6ea960e5292d9741cad890e42a72d347955
harsh <harsh23@groovyweb.co>
2023-07-13 18:25:25 +0530

    🌔 : css changes

--- frontend/views/user/register.php
+++ frontend/views/user/register.php
@@ -8,8 +8,40 @@ AppAsset::register($this);
 $this->title = 'Register';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+<style>
+ 
 
-<h1><?= Html::encode($this->title) ?></h1>
+    #top main div {
+        display: grid;
+        justify-content: center;
+    }
+
+    .user-register {
+        background: aliceblue;
+        padding: 2.5rem;
+        border: 1px solid midnightblue;
+        border-radius: 1rem;
+        display: flex;
+        width: fit-content;
+    }
+
+    input[type="text"],
+    input[type="password"],
+    input[type="email"] {
+        border: none;
+    }
+
+    .button {
+        width: 100%;
+        border: none;
+        margin: 1rem 0rem;
+        border-radius: 0.2rem;
+        padding: 0.5rem;
+    }
+</style>
+<h1>
+    <?= Html::encode($this->title) ?>
+</h1>
 
 <div class="user-register">
     <?php $form = ActiveForm::begin(); ?>
@@ -21,7 +53,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <?= $form->field($model, 'password')->passwordInput(['maxlength' => true]) ?>
 
     <div class="form-group">
-        <?= Html::submitButton('Register', ['class' => 'btn btn-primary']) ?>
+        <?= Html::submitButton('Register', ['class' => 'button']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
